7600c441bbf10bdcc5c7d40ad3f73826d18c96de
Mike <mike@pop-os.localdomain>
2020-07-14 21:07:20 -0400

    Added char_to_g_map_creator. I suck at loop macros still

--- ggg/commonlisp/gggTranslator.lisp
+++ ggg/commonlisp/gggTranslator.lisp
@@ -24,3 +24,20 @@
       (cons (take size l) (chunk size (drop size l)))
       '()))
   
+
+(defvar *alphaNumAll*
+  (append
+   (loop for i from 65 to 122 when (alphanumericp (code-char i)) collect (code-char i))
+   (loop for i from 0 to 9 collect (digit-char i))))
+
+(defun char_to_g_map_creator (msg)
+  (let* ((unique-letters (remove-if-not #'alphanumericp
+					 (remove-duplicates msg)))
+	 (letter-and-ix (mapcar #'list
+				(loop for x across unique-letters collect x)
+				(loop for i from 4 to (+ 3 (length unique-letters))
+				      collect (inttogggiter i))))
+	 (h (make-hash-table)))
+    (loop for (c g) in letter-and-ix do (setf (gethash c h) g))
+    h))
+
