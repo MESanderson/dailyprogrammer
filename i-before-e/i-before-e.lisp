72c5a19e023b09886dbf97bdcc6faca45cf8cda9
Mike <mike@pop-os.localdomain>
2020-12-07 20:15:07 -0500

    fixed case sensitivity

--- i-before-e/i-before-e.lisp
+++ i-before-e/i-before-e.lisp
@@ -1,5 +1,5 @@
 (ql:quickload "cl-ppcre")
 
-(defparameter exception-regex (ppcre:create-scanner "([^cC]ei|cie)"))
+(defparameter exception-regex (ppcre:create-scanner "([^c]ei|cie)"))
 (defun i-before-e-p (string-in)
-  (null (ppcre:scan exception-regex string-in)))
+  (null (ppcre:scan exception-regex (string-downcase string-in))))
