2e2e7706a53719f427363f6b3e91b3d2da010743
Mike Sanderson <MESanderson@users.noreply.github.com>
2022-05-24 17:20:05 -0600

    Refactored a bit and added documentation

--- ggg/gggTranslator.hs
+++ ggg/gggTranslator.hs
@@ -11,43 +11,66 @@ tst_hdr = "H GgG d gGg e ggG l GGg o gGG r Ggg w ggg"
 tst_msg = "GgGggGGGgGGggGG, ggggGGGggGGggGg!"
 tst_rev_msg = "Hello, world!"
 
-ggg_list_tokenizer (a:b:xs) = [(a, b)] ++ ggg_list_tokenizer xs
-ggg_list_tokenizer [] = []
+-- |chunk function to group list into list of n element lists
+-- similar to simple version of clojure's partition
+chunk :: Int -> [a] -> [[a]]
+chunk _ [] = []
+chunk size list
+  | size > 0 = (take size list) : chunk size (drop size list)
+  | otherwise = []
+
+-- Simple function to turn lists of size 2 to 2 element tuples
+list_to_tup2 :: [a] -> (a, a)
+list_to_tup2 [a,b] = (a,b)
+
 
 alphaNumAll = [chr x | x <- [65..122], isAlphaNum $ chr x] ++ [intToDigit x | x <- [0..9]]
 
+-- letter to ggg value map creator
+letter_to_ggg_map hdr_str = Map.fromList $ map list_to_tup2 (chunk 2  $ words hdr_str)
 
+-- ggg value to letter map creator
+ggg_to_letter_map hdr_str = Map.fromList $ map Tuple.swap $
+  map list_to_tup2 (chunk 2 $ words hdr_str)
 
-letter_to_ggg_map hdr_str = Map.fromList $ ggg_list_tokenizer $ words hdr_str
-ggg_to_letter_map hdr_str = Map.fromList $ map Tuple.swap $ ggg_list_tokenizer $ words hdr_str
-
-
+-- |Function for grouping by contiguous values "G/g" or non "G/g"
+-- example: g_chunker "gGggOhnomoreGs" = ["gGgg", "Ohnomore", "G", "s"]
+g_chunker :: String -> [String]
 g_chunker str = groupBy chunkP str
    where chunkP = (\ x y -> (toLower x == 'g') == (toLower y == 'g'))
 
-
+-- |Function to decode a string of consecutive G/g values using a
+-- Map of G/g keys to letter values
+translate_g_chunk :: Map.Map [Char] [Char] -> [Char] -> [Char]
 translate_g_chunk _ "" = ""
 translate_g_chunk trnsMap str
-  | startsWithG = Map.findWithDefault "*" (take gChunkSize str) trnsMap ++ translate_g_chunk trnsMap (drop gChunkSize str)
+  | startsWithG = Map.findWithDefault "*" (take gChunkSize str) trnsMap ++
+                    translate_g_chunk trnsMap (drop gChunkSize str)
   | otherwise = str
   where startsWithG = 'g' == (toLower $ head str)
         gChunkSize = (length . fst . head. Map.toList) trnsMap
 
 
+-- |Function to encode string broken into consecutive 'g/G' values or non 'g/G' values
+-- example: ["GgGggG", ", ", "ggGGGg", "!"] = "Oh, Hi!"
+g_rev_translater :: Map.Map [Char] [Char] -> [Char] -> [Char]
 g_rev_translater gToCharMap str =
   concatMap (translate_g_chunk gToCharMap) chunked
   where chunked = g_chunker str
 
 
-char_to_g_translater :: Map.Map [Char] [Char] -> [Char] -> [Char]
-char_to_g_translater trnsMap "" = ""
-char_to_g_translater trnsMap str
-  | is_trns = Map.findWithDefault "_" first_ltr trnsMap ++ char_to_g_translater trnsMap (tail str)
-  | otherwise = first_ltr ++ char_to_g_translater trnsMap  (tail str)
+-- |Function to encode message to g/G message
+g_encoder :: Map.Map [Char] [Char] -> [Char] -> [Char]
+g_encoder charToGMap "" = ""
+g_encoder charToGMap str
+  | is_trns = Map.findWithDefault "_" first_ltr charToGMap ++
+                g_encoder charToGMap (tail str)
+  | otherwise = first_ltr ++ g_encoder charToGMap  (tail str)
   where first_ltr = take 1 str
-        is_trns = Map.member first_ltr trnsMap 
+        is_trns = Map.member first_ltr charToGMap 
 
-
+-- |Function to turn integers into binary 'g/G' values (0='g', 1='G') string
+intTogggIter :: Integer -> [Char]
 intTogggIter i
   | i == 0 = ""
   | r == 1 = intTogggIter d ++ "G"
@@ -55,6 +78,8 @@ intTogggIter i
   where (d, r) = divMod i 2
 
 
+-- |Function to produce a char to G/g Map based on unique characters
+char_to_g_map_creator :: [Char] -> Map.Map [Char] [Char] 
 char_to_g_map_creator msg =
   Map.fromList $ zip uniqueLettersString gggIter 
   where uniqueLetters = filter isAlphaNum $ Set.toList (Set.fromList msg)
@@ -67,7 +92,7 @@ char_to_g_map_creator msg =
 exampleMsg =  "I did not! Oh hai mark."
 exampleChToGMap = char_to_g_map_creator exampleMsg
 exampleGToChMap = Map.fromList ([(b, a) | (a,b) <- Map.toList exampleChToGMap])
-exampleGggEncode = char_to_g_translater exampleChToGMap exampleMsg
+exampleGggEncode = g_encoder  exampleChToGMap exampleMsg
 exampleRevEncode = g_rev_translater exampleGToChMap exampleGggEncode
 exampleTrue = exampleMsg == exampleRevEncode
 
