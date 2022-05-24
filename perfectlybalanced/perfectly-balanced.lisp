5c676c0a80955759a712ee3393fa1203c2d51c30
Mike Sanderson <MESanderson@users.noreply.github.com>
2022-05-24 17:20:05 -0600

    Fixed unit test and bonus. also added second way using libraries

--- perfectlybalanced/perfectly-balanced.lisp
+++ perfectlybalanced/perfectly-balanced.lisp
@@ -1,6 +1,21 @@
-(defun balanced (str)
+(ql:quickload "alexandria")
+
+(defun balanced (str &optional (xs 0) (ys 0))
+  (if (equal str "")
+      (equal xs ys)   
+      (let ((char1 (char str 0))
+	    (str-rest (subseq str 1)))
+      (cond ((equal #\x char1) (balanced str-rest (1+ xs) ys))
+	    ((equal #\y char1) (balanced str-rest xs (1+ ys)))))))
+  
+(defun balanced_bonus (str)
   (let ((h (make-hash-table)))
     (progn
       (loop for i across str do (setf (gethash i h) (1+ (gethash i h 0))))
-      (equal 1 (length (remove-duplicates (loop for v being the hash-values of h collect v)))))))
-	
+      (>= 1 (length (remove-duplicates (loop for v being the hash-values of h collect v)))))))
+
+(defun balanced_bonus2 (str)
+  (let ((h (make-hash-table)))
+    (progn
+      (loop for i across str do (setf (gethash i h) (1+ (gethash i h 0))))
+      (<= 1 (length (remove-duplicates (alexandria:hash-table-values h)))))))
